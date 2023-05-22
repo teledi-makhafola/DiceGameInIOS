c9d2f47cfd88bd27ee081d14d21aef188780dfb8
teledi_makhafola <lkgahliso03@gmail.com>
2023-05-22 16:34:22 +0200

    first commit

--- DiceAppAssessment/ViewController.swift
+++ DiceAppAssessment/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func ButtonPressed(_ sender: Any) {
+        var array = [#imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6")]
+        
+        diceImage1.image = array[ Int.random(in: 0...5)]
+        
+        diceImage2.image = array[ Int.random(in: 0...5)]
+      
+    }
 }
 
